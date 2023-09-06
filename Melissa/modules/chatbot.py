2863552beedc278080be230cee445a8b6e67ac68
Nicky Lalrochhara <nickylalrochhara917@gmail.com>
2023-09-07 00:14:30 +0530

    Add files via upload

---
+++ Melissa/modules/chatbot.py
@@ -0,0 +1,169 @@
+import html
+import json
+import re
+from time import sleep
+
+import requests
+from telegram import (
+    CallbackQuery,
+    Chat,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    ParseMode,
+    Update,
+    User,
+)
+from telegram.ext import (
+    CallbackContext,
+    CallbackQueryHandler,
+    CommandHandler,
+    Filters,
+    MessageHandler,
+)
+from telegram.utils.helpers import mention_html
+
+import Melissa.modules.sql.chatbot_sql as sql
+from Melissa import BOT_ID, BOT_NAME, BOT_USERNAME, dispatcher
+from Melissa.modules.helper_funcs.chat_status import user_admin, user_admin_no_reply
+from Melissa.modules.log_channel import gloggable
+
+
+@user_admin_no_reply
+@gloggable
+def Melissarm(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"rm_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_Melissa = sql.set_Melissa(chat.id)
+        if is_Melissa:
+            is_Melissa = sql.set_Melissa(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_DISABLED\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+
+@user_admin_no_reply
+@gloggable
+def Melissaadd(update: Update, context: CallbackContext) -> str:
+    query: Optional[CallbackQuery] = update.callback_query
+    user: Optional[User] = update.effective_user
+    match = re.match(r"add_chat\((.+?)\)", query.data)
+    if match:
+        user_id = match.group(1)
+        chat: Optional[Chat] = update.effective_chat
+        is_Melissa = sql.rem_Melissa(chat.id)
+        if is_Melissa:
+            is_Melissa = sql.rem_Melissa(user_id)
+            return (
+                f"<b>{html.escape(chat.title)}:</b>\n"
+                f"AI_ENABLE\n"
+                f"<b>Admin :</b> {mention_html(user.id, html.escape(user.first_name))}\n"
+            )
+        else:
+            update.effective_message.edit_text(
+                "{} ᴄʜᴀᴛʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ {}.".format(
+                    dispatcher.bot.first_name, mention_html(user.id, user.first_name)
+                ),
+                parse_mode=ParseMode.HTML,
+            )
+
+    return ""
+
+
+@user_admin
+@gloggable
+def Melissa(update: Update, context: CallbackContext):
+    message = update.effective_message
+    msg = "• ᴄʜᴏᴏsᴇ ᴀɴ ᴏᴩᴛɪᴏɴ ᴛᴏ ᴇɴᴀʙʟᴇ/ᴅɪsᴀʙʟᴇ ᴄʜᴀᴛʙᴏᴛ"
+    keyboard = InlineKeyboardMarkup(
+        [
+            [
+                InlineKeyboardButton(text="ᴇɴᴀʙʟᴇ", callback_data="add_chat({})"),
+                InlineKeyboardButton(text="ᴅɪsᴀʙʟᴇ", callback_data="rm_chat({})"),
+            ],
+        ]
+    )
+    message.reply_text(
+        text=msg,
+        reply_markup=keyboard,
+        parse_mode=ParseMode.HTML,
+    )
+
+
+def Melissa_message(context: CallbackContext, message):
+    reply_message = message.reply_to_message
+    if message.text.lower() == "Melissa":
+        return True
+    elif BOT_USERNAME in message.text:
+        return True
+    elif reply_message:
+        if reply_message.from_user.id == BOT_ID:
+            return True
+    else:
+        return False
+
+
+def chatbot(update: Update, context: CallbackContext):
+    message = update.effective_message
+    chat_id = update.effective_chat.id
+    bot = context.bot
+    is_Melissa = sql.is_Melissa(chat_id)
+    if is_Melissa:
+        return
+
+    if message.text and not message.document:
+        if not Melissa_message(context, message):
+            return
+        bot.send_chat_action(chat_id, action="typing")
+        request = requests.get(
+            f"https://kora-api.vercel.app/chatbot/2d94e37d-937f-4d28-9196-bd5552cac68b/{BOT_NAME}/famhawiteinfosys/message={message.text}"
+        )
+        results = json.loads(request.text)
+        sleep(0.5)
+        message.reply_text(results["reply"])
+
+
+__help__ = f"""
+*{BOT_NAME} has an chatbot which provides you a seemingless chatting experience :*
+
+ »  /chatbot *:* Shows chatbot control panel
+"""
+
+__mod_name__ = "Cʜᴀᴛʙᴏᴛ"
+
+
+CHATBOTK_HANDLER = CommandHandler("chatbot", Melissa, run_async=True)
+ADD_CHAT_HANDLER = CallbackQueryHandler(Melissaadd, pattern=r"add_chat", run_async=True)
+RM_CHAT_HANDLER = CallbackQueryHandler(Melissarm, pattern=r"rm_chat", run_async=True)
+CHATBOT_HANDLER = MessageHandler(
+    Filters.text
+    & (~Filters.regex(r"^#[^\s]+") & ~Filters.regex(r"^!") & ~Filters.regex(r"^\/")),
+    chatbot,
+    run_async=True,
+)
+
+dispatcher.add_handler(ADD_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOTK_HANDLER)
+dispatcher.add_handler(RM_CHAT_HANDLER)
+dispatcher.add_handler(CHATBOT_HANDLER)
+
+__handlers__ = [
+    ADD_CHAT_HANDLER,
+    CHATBOTK_HANDLER,
+    RM_CHAT_HANDLER,
+    CHATBOT_HANDLER,
+]
